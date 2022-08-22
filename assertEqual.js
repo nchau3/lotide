ff8c958c00a3018ea73ba2a30c370bd932f095ba
Nicholas <nc_chau@hotmail.com>
2022-08-22 18:07:53 +0000

    updated with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
   } else if (actual !== expected) {
-    console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
