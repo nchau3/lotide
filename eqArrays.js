0df2447aad753f37196311ac3b248e0bf60f1768
Nicholas <nc_chau@hotmail.com>
2022-08-24 16:33:47 +0000

    fixed bug, now accounts for array length

--- eqArrays.js
+++ eqArrays.js
@@ -8,6 +8,9 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
   let result = true;
+  if (array1.length !== array2.length) {
+    result = false;
+  };
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       result = false;
@@ -16,6 +19,6 @@ const eqArrays = function(array1, array2) {
 };
 
 assertEqual(eqArrays(["test", "testing"], ["test", "testing"]), true); //pass
-assertEqual(eqArrays(["test", "testing"], ["test", "resting"]), true); //fail
+assertEqual(eqArrays(["test", "testing"], ["test", "resting"]), false); //fail
 assertEqual(eqArrays([450, 956], [450, 956]), true); //pass
-assertEqual(eqArrays([450, 956], [450, '956']), true); //fail
+assertEqual(eqArrays([450, 956], [450, '956']), false); //fail
