50f4f5cfb8db2cecd000cb70115f707a7629d5de
Nicholas <nc_chau@hotmail.com>
2022-08-28 17:58:49 +0000

    reworked eqObjects to account for nested objects (recursion), updated assertObjectsEqual to match

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -10,24 +10,58 @@ const eqArrays = function(array1, array2) {
   } return result;
 };
 
+//checks if an entry is an object (not array or null)
+const isObject = function(input) {
+  if ((typeof input === 'object') && (Array.isArray(input) === false) && (input !== null)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//alphabetize elements in array, from MDN docs on array.sort
+const alphabetize = array => array.sort((a, b) => {
+  if (a < b) {
+    return -1;
+  }
+  if (a > b) {
+    return 1;
+  }
+  return 0;
+});
+
+//returns all keys of given object as a string, in alphabetical order (accounts for same keys/values in different orders)
+const getKeys = object => alphabetize(Object.keys(object)).join('');
+
 const eqObjects = function(object1, object2) {
   let result = true;
-  const keysLength1 = Object.keys(object1).length;
-  const keysLength2 = Object.keys(object2).length;
-  if (keysLength1 !== keysLength2) {
+  let keys1 = getKeys(object1);
+  let keys2 = getKeys(object2);
+  //not same keys? - return FALSE
+  if (keys1 !== keys2) {
     result = false;
-  } else if (keysLength1 === keysLength2) {
-    for (const key in object1) {
+    return result;
+  }
+  for (const key in object1) {
+    //key value is not an object? compare values
+    if (!isObject(object1[key])) {
+    //is array? NO
       if (!Array.isArray(object1[key])) {
-        if (object1[key] !== object2[key]) {
+        //values not the same? return FALSE
+        if ((object1[key] !== object2[key])) {
           result = false;
+          return result;
         }
-      } else if (Array.isArray(object1[key])) {
-        if (eqArrays(object1[key], object2[key]) === false) {
+        //is array? compare array values
+      } else {
+        if (!eqArrays(object1[key], object2[key])) {
           result = false;
         }
       }
-    }  
+    //value is object? initiate RECURSION, store return value in original "result"
+    } else {
+      result = eqObjects(object1[key], object2[key]);
+    }
   } return result;
 };
 
--- eqObjects.js
+++ eqObjects.js
@@ -6,11 +6,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+//compares values of 2 arrays
 const eqArrays = function(array1, array2) {
   let result = true;
   if (array1.length !== array2.length) {
     result = false;
-  };
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       result = false;
@@ -18,27 +19,62 @@ const eqArrays = function(array1, array2) {
   } return result;
 };
 
+//checks if an entry is an object (not array or null)
+const isObject = function(input) {
+  if ((typeof input === 'object') && (Array.isArray(input) === false) && (input !== null)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//alphabetize elements in array, from MDN docs on array.sort
+const alphabetize = array => array.sort((a, b) => {
+  if (a < b) {
+    return -1;
+  }
+  if (a > b) {
+    return 1;
+  }
+  return 0;
+});
+
+//returns all keys of given object as a string, in alphabetical order (accounts for same keys/values in different orders)
+const getKeys = object => alphabetize(Object.keys(object)).join('');
+
 const eqObjects = function(object1, object2) {
   let result = true;
-  const keysLength1 = Object.keys(object1).length;
-  const keysLength2 = Object.keys(object2).length;
-  if (keysLength1 !== keysLength2) {
+  let keys1 = getKeys(object1);
+  let keys2 = getKeys(object2);
+  //not same keys? - return FALSE
+  if (keys1 !== keys2) {
     result = false;
-  } else if (keysLength1 === keysLength2) {
-    for (const key in object1) {
+    return result;
+  }
+  for (const key in object1) {
+    //key value is not an object? compare values
+    if (!isObject(object1[key])) {
+    //is array? NO
       if (!Array.isArray(object1[key])) {
-        if (object1[key] !== object2[key]) {
+        //values not the same? return FALSE
+        if ((object1[key] !== object2[key])) {
           result = false;
+          return result;
         }
-      } else if (Array.isArray(object1[key])) {
-        if (eqArrays(object1[key], object2[key]) === false) {
+        //is array? compare array values
+      } else {
+        if (!eqArrays(object1[key], object2[key])) {
           result = false;
         }
       }
-    }  
+    //value is object? initiate RECURSION, store return value in original "result"
+    } else {
+      result = eqObjects(object1[key], object2[key]);
+    }
   } return result;
 };
 
+/*
 //TEST CODE
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
@@ -53,3 +89,12 @@ assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
+
+const test1 = { a: { f: 1 }, b: 3, c: { d: { e : 1}}};
+const test2 = { a: { f: 1 }, b: 3, c: { d: { e : 1}}};
+const test3 = { a: { y: 0, z: 1 }, b: 2 };
+const test4 = { a: 1, b: 2 };
+
+assertEqual(eqObjects(test1, test2), true);
+assertEqual(eqObjects(test1, test3), false);
+assertEqual(eqObjects(test3, test4), false); */
