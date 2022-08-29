e107cf73a521e6f9e4f8ce635a60dffe035989ac
Nicholas <nc_chau@hotmail.com>
2022-08-29 14:24:11 +0000

    npm init, converted headTest to mocha tests

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,17 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-const testArray1 = [1, 3, 5, 4];
-const testArray2 = [];
+describe("#head", () => {
+  
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-assertEqual(head(testArray1), 1);
-assertEqual(head(testArray2), undefined);
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 'undefined' for []", () => {
+    assert.strictEqual(head([]), undefined); 
+  });
+});
